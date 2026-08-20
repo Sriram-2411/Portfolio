62acd276ee4d3339b7c6dabd7346c35e95c2211b
Sriram-2411 <sriram24112001@gmail.com>
2026-08-20 11:58:29 +0530

    Update portfolio

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -152,7 +152,7 @@ export const Hero = () => {
   </a>
 
   <a
-       href="https://www.linkedin.com/in/sriram-saba540247/"
+       href="https://www.linkedin.com/in/sriram-s-5a7934324/"
        target="_blank"
       rel="noopener noreferrer"
     className="w-10 h-10 rounded-full border border-border flex items-center justify-center
